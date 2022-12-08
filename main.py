0f3a86cbf08f74645eab9e02b9be3239830f3446
Leandro Santana de Castro <lde12@lifescan.com>
2022-12-07 21:07:28 -0300

    "Initial Commit"

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///frangoAcademia.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
+
 @app.route("/")
 def homepage():
     sucesso = request.args.get("sucesso", default=False)
     return render_template("homepage.html", sucesso=sucesso)
 
+
 with app.app_context():
     db.create_all()
+
 
 @app.route("/contatos")
 def contatos():
     return render_template("contatos.html")
+
 
 @app.route('/create', methods=['POST'])
 def create():
@@ -30,10 +34,12 @@ def create():
     db.session.commit()
     return redirect("/?sucesso=true")
 
+
 @app.route("/usuarios")
 def usuarios():
     alunos = User.query.all()
     return render_template("usuarios.html", alunos=alunos)
+
 
 @app.route("/usuarios/excluir/<int:id>")
 def excluir(id):
@@ -47,18 +53,27 @@ def excluir(id):
 def editar(id):
     aluno = User.query.filter_by(id=id).first()
 
-    if request.method == 'POST':
-        if aluno:
-            name = request.form['name']
-            telefone = request.form['telefone']
-            email = request.form['email']
-            objetivo = request.form['objetivo']
+    if aluno:
+        nome = request.form['nome']
+        telefone = request.form['telefone']
+        email = request.form['email']
+        objetivo = request.form['objetivo']
 
-            aluno = User(nome=name, telefone=telefone, email=email, objetivo=objetivo)
+        aluno.nome = nome
+        aluno.telefone = telefone
+        aluno.email = email
+        aluno.objetivo = objetivo
+        db.session.commit()
+        return redirect("/usuarios")
+    return f"O aluno com o  = {id} não existe"
 
-            db.session.edit(aluno)
-            db.session.commit()
-            return redirect("/create")
+@app.route("/usuarios/editar/<int:id>", methods=['GET'])
+def mostrar(id):
+    aluno = User.query.filter_by(id=id).first()
+    if aluno == None:
+        return f" O aluno {id} não existe"
+    return render_template('update.html', aluno=aluno)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
